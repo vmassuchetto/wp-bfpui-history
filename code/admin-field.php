8e09b6ca8145d0c54b460987dae7097a28818ee2
Vinicius Massuchetto <vmassuchetto@gmail.com>
2013-11-23 02:27:27 -0200

    Apresentação para o WordCamp Sampa 2013

--- code/admin-field.php
+++ code/admin-field.php
@@ -4,9 +4,16 @@ if ( is_admin() ) {
 }
 function admin_init() {
     add_settings_field( self::$option_name, __( 'Post Home Page', 'better_front_page_ui' ), array( $this, 'output_setting_form' ), 'permalink', 'optional' );
+    global $pagenow;
+    if ( $pagenow == 'options-permalink.php' ) {
+        if ( isset( $_POST[self::$option_name] ) ) {
+            $value = $this->sanitize_option( $_POST[self::$option_name] );
+            update_option( self::$option_name, $value );
+        } else {
+            delete_option( self::$option_name );
+        }
+    }
 }
 function output_setting_form() {
-    ?>
-    <!-- html do campo aqui -->
-    <?php
+    ?><!-- html do campo aqui --><?php
 }
